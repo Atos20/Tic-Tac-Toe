87d5158f18352adfb8b94a3c938d387ac98f9dd0
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 15:44:34 -0600

    Add methods to game.js

--- src/game.js
+++ src/game.js
@@ -1,17 +1,20 @@
 const Player = require('../src/player');
 class Game{
     constructor() {
-        this.player1 = new Player ('1');
-        this.player2 = new Player ('2');
-        this.turns = 0;
-        this.courrentTurn = 'player1'
+        this.player1 = new Player ('1');//tested
+        this.player2 = new Player ('2');// tested
+        this.turns = 0; //tested
+        this.courrentTurn = 'player1';
+        this.announcement = ''; //tested
+        this.whoseTurn = this.player1;
+
         this.gameBoard = [  //=> to check for the token being placed by the player
             '', '', '',     //=> [0] topleft position
             '', '', '',
             '', '', ''
         ];
       
-        this.winningCondition = [
+        this.possibleWinnings = [
             [0, 1, 2], //checks for the top row
             [3, 4, 5], //checks for the middle row
             [6, 7, 8], // checks for the botton row
@@ -19,11 +22,27 @@ class Game{
             [1, 4, 7], // checks for the middle column 
             [2, 5, 8], // checks for the right column
             [0, 4, 8], // checks 1st cross
-            [2, 4, 6] // checks 2nd cross
+            [2, 4, 6]  // checks 2nd cross
         ];
     }
+
     swapTurns() {
-        console.log('hi')
+        
     }
+
+    placeToken(){
+        //being able to provide tokens to the the board => this function willbe ablo to change the elements on the board
+    }
+
+    updateAnnouncement(player){//needs to make use of a paramter so it can interpolate the player
+        this.announcement = `It's ${player.name} turn`
+        return this.announcement;
+    }
+
+    checkForWin(){
+
+    }
+
+
 }
 module.exports = Game;
