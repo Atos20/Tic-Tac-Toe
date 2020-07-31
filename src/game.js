dca5cbd57733f1edf0b48fc660c76897f05659f6
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 17:51:50 -0600

    Revaluate place toke function

--- src/game.js
+++ src/game.js
@@ -1,20 +1,14 @@
 // const Player = require('../src/player');
 
 class Game{
-    constructor() {
+    constructor(stateBoard) {
         this.player1 = new Player ('1');//tested
         this.player2 = new Player ('2');// tested
         this.turns = 0; //tested
         this.courrentTurn = 'player1';
-        this.announcement = ''; //tested
+        this.announcement = ''; //tested => will display the title
         this.whoseTurn = this.player1;
-
-        this.gameBoard = [  //=> to check for the token being placed by the player
-            '', '', '',     //=> [0] topleft position
-            '', '', '',
-            '', '', ''
-        ];
-
+        this.gameBoard = stateBoard//=> to check for the token being placed by the player   //=> [0] topleft position
         this.possibleWinnings = [
             [0, 1, 2], //checks for the top row
             [3, 4, 5], //checks for the middle row
@@ -26,9 +20,7 @@ class Game{
             [2, 4, 6]  // checks 2nd cross
         ];
     }
-
     
-
     printBoard() {
         let formattedString = '';
         this.gameBoard.forEach((cell, index) => {
@@ -42,23 +34,22 @@ class Game{
         console.log('%c' + formattedString, 'color: red ;font-size:20px');
     }
 
-    
 
-    placeToken(player){
-        if(player.id === '1' && currentTurn === 'player1') {
-            
-    }
+    // placeToken(player){
+    //     if(player.id === '1' && currentTurn === 'player1') {
+    //         //I think this method could be better use as a funtion that manipuates the data.
+    // }
 
-        //being able to provide tokens to the the board => this function willbe ablo to change the elements on the board
+    //     //being able to provide tokens to the the board => this function willbe ablo to change the elements on the board
+    // }
+
+    checkForWin(){
+
     }
 
     updateAnnouncement(player){//needs to make use of a paramter so it can interpolate the player
         this.announcement = `It's ${player.name} turn`
         return this.announcement;
-    }
-
-    checkForWin(){
-
     }
 
 
