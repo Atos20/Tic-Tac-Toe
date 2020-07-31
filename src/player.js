67c75620b70a22b2cd08dbf2d0830edb4097e11b
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 11:48:08 -0600

    Write player testing

--- src/player.js
+++ src/player.js
@@ -1,6 +1,25 @@
 class Player{
-    constructor(){
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.winCount = 0;
+        this.gameLost = false;
+    }
 
+    addWins(){
+        this.winCount++
+    }
+
+    victoryLost() {
+        this.gameLost = !this.gameLost
+    }
+
+    saveWinsToStorage(){
+
+    }
+
+    retrieveWinsFromStorage(){
+        
     }
 }
 
