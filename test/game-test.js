854787bc759ff9ce69d580b104e2a3605a368f25
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 13:50:55 -0600

    Write testing for player instances

--- test/game-test.js
+++ test/game-test.js
@@ -7,4 +7,14 @@ describe('Game', () => {
         assert.isFunction(Game);
     
       });
+    
+    it('should be able to instantiate only two player', () => {
+        const game = new Game();
+    
+        assert.equal(game.player1.id,  "1");
+        assert.equal(game.player2.id, "2");
+        assert.typeOf(game.player1, 'object');
+        assert.typeOf(game.player2, 'object');
+      });
+    
     });
