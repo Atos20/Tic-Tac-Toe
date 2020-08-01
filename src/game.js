e7a36d1815854ba74a36ca395cab51c741bc9e58
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 19:44:53 -0600

    refactor checkForReadiness method

--- src/game.js
+++ src/game.js
@@ -45,18 +45,23 @@ class Game{
     }
 
 
-    gameStatus(){
+    checkReadiness(){
         //check for game readiness
         for (var i = 0; i < this.gameBoard.length; i++){
-            console.log(this.gameBoard[i] === '')
-            if (this.gameBoard[i] === ''){
+            console.log(this.gameBoard[i])
+            if (!this.gameBoard[i]){ //An empty string is a falsy value
+                console.log('check if the board is empty')
                 return true;
                 //I can use this boolean to handle the logic of the game
             }
         }
-        //check for draw, if it is full it is a draw
-        //check for win => if it meets one of the conditions is a win
     }
+
+    checkForDraws() {
+
+        //check for draw, if it is full it is a draw
+    }
+        //check for win => if it meets one of the conditions is a win
 
     updateAnnouncement(player){//needs to make use of a paramter so it can interpolate the player
         this.announcement = `It's ${player.name} turn`
