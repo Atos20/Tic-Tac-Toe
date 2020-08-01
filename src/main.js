8bc7331081418f1b882449fb0e48c33b7fcc0668
Orlando Murcio <ato.murcio@gmail.com>
2020-07-31 18:01:53 -0600

    move placeToke function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
 var board = ['', '' , '', '', '', '', '', '', '']
 var game = new Game(board )
 console.log(game.printBoard( ))
+
+
+  // placeToken(player){
+    //     if(player.id === '1' && currentTurn === 'player1') {
+    //         //I think this method could be better use as a funtion that manipuates the data.
+    // }
+
+    //     //being able to provide tokens to the the board => this function willbe ablo to change the elements on the board
+    // }
