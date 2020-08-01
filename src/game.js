752a77921b188546043a1f9e867a28d9cff9baff
Orlando Murcio <ato.murcio@gmail.com>
2020-08-01 11:36:39 -0600

    Refactor findCurrentTurn

--- src/game.js
+++ src/game.js
@@ -5,7 +5,8 @@ class Game{
         this.player1 = new Player ('1');//tested
         this.player2 = new Player ('2');// tested
         this.gameBoard = boardState//=> to check for the token being placed by the player   
-        this.courrentTurn;
+        this.gameTurns = 0
+        this.currentTurn;
         this.announcement = ''; //tested => will display the title
         // this.whoseTurn = this.player1;
         this.possibleWinnings = [
@@ -47,61 +48,49 @@ class Game{
         })
     }
 
-    checkForVictories(){
+    checkForVictories(){//=> this will run at the fifth turn ad up 
         //check for readiness???
-        //check for win => if it meets one of the conditions is a win
-        //when a game is won playes cannot place token on the grid
+        //forloop 
         if (this.gameBoard[0]=== this.gameBoard[1] && this.gameBoard[0] === this.gameBoard[2]){
              //     [0, 1, 2], //checks for the top row
-            console.log('the winner is =>', this.gameBoard[0])
-            // this.resetGame();
-            return;
+            return true;
         }
         if (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[3] === this.gameBoard[5]){
             //     [3, 4, 5], //checks for the middle row
-            console.log('the winner is => ',this.gameBoard[3])
-            // this.resetGame();
-            return;
+            return true;
         }
         if (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[6] === this.gameBoard[8]){
             //   [6, 7, 8], // checks for the botton row
-            console.log('the winner is => ', this.gameBoard[6])
-            // this.resetGame();
-            return;
+            return true;
         }
         if (this.gameBoard[0]  === this.gameBoard[3]&& this.gameBoard[0] === this.gameBoard[6]){
             //     [0, 3, 6], // checks for the left colum
-            console.log('the winner is => ', this.gameBoard[0])
-            // this.resetGame();
-            return;
+            return true;
         }
         if (this.gameBoard[1] === this.gameBoard[4]&& this.gameBoard[1]=== this.gameBoard[7]){
             //     [1, 4, 7], // checks for the middle column 
-            console.log('the winner is =>', this.gameBoard[1])
-            // this.resetGame();
-            return;
+            return true;
         }
-        if (this.gameBoard[2] === this.gameBorad[5] && this.gameBoard[2] === this.gameBoard[8]){
+        if (this.gameBoard[2] === this.gameBoard[5] && this.gameBoard[2] === this.gameBoard[8]){
             //     [2, 5, 8], // checks for the right column
-            console.log('the winner is =>', this.gameBoard[2])
-            // this.resetGame();
-            return;
+            return true;
         }
-        
         if ( this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[0] === this.gameBoard[8]){
-            //     [0, 4, 8], // checks 1st cross
-            console.log('the winner is =>', this.gameBoard[0])
-            // this.resetGame(); 
-            return;
+            //     [0, 4, 8], // checks 1st cross 
+            return true;
         }
         if (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[2] === this.gameBoard[6]){
             //     [2, 4, 6]  // checks 2nd cross
-            console.log('the winner is =>', this.gameBoard[2])
-            // this.resetGame(); 
-            return;
+            return true;
         }
     }
     
+    checkWin(){
+        if ( this.gameTurns >= 5){
+            this.checkForVictories()
+        }
+    }
+
     updateHeader(player){//needs to make use of a paramter so it can interpolate the player
        if (checkForVictories()){
            this.announcement = `It's ${player.name} turn`
@@ -109,37 +98,44 @@ class Game{
        }
     }
 
-    insertToken(player, token, position){
-        if (position > 8 || this.gameBoard[position] > 8 || this.gameBoard[position] !== ''){//=> it checks for two conditions 
+    insertToken(token, position){//=>hardcode the tokens :) 
+        //decrements the Game turns
+        
+        if (position > 8 || this.gameBoard[position] > 8 || this.gameBoard[position] !== ''){//=> it checks for the length of array and overriding tokens
             return false;
         } else {
+            this.gameTurns++
+            console.log('turns left', 9 - this.gameTurns )
             this.gameBoard[position] = token
-            this.findCourrentTurn(player)
-            console.log('player ' + this.findCourrentTurn(player) + 'has placed a ' + token + 'at position ' + this.gameBoard[position])
+            this.findCourrentTurn()
+            //checkForVictory
+            console.log('player ' + this.findCourrentTurn() + 'has placed a ' + token + 'at position ' + this.gameBoard[position])
             this.printBoard()
             return true;
         }
     }
 
-    findCourrentTurn(player) { 
-        if (player === this.player1){
-            return this.player1.id //=> remove id's these are just for testing 
+    findCourrentTurn() { 
+        if (this.gameTurns %2 === 0){
+            return this.player2.id //=> remove id's these are just for testing 
         } else {
-            return this.player2.id
+            return this.player1.id
         }
     }
 
 
     findWinner(player){//The winner will be founf and then the winCount will incease
-        var winner;
+        var winner; //=> p1 or p2 
         console.log(player)
-        if (player.lostGame === true){
-
-        }
         //should find who has got a victory
-        
-        //once it finds the winner, it invokes the method that increases the win count
-
+        // if (player.lostGame){//=> true
+            
+        // }   
+        // winner = player
+        //once it finds the winner, it invokes the method that increases the win count for that specific
+        // player.addWins()//=>
+        // player.saveWinsToStorage()
+        // how to save to LS the win
     }
 
 
