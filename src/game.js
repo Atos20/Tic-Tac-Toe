11892ed0f341f8b33438927f05af7ab7c631085f
Orlando Murcio <ato.murcio@gmail.com>
2020-08-01 07:59:00 -0600

    Fix victories logic

--- src/game.js
+++ src/game.js
@@ -31,81 +31,84 @@ class Game{
             }
         });
         console.log('%c' + formattedString, 'color: red ;font-size:20px');
-        console.log(`\n\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015\n`)
     }
 
-    takeTurn() { //refactor this using a ternerary
-        
+    takeTurn() { 
         this.turns = !this.turns;
         this.turns ? this.courrentTurn = 'player1' : this.courrentTurn = 'player2';
-        console.log(this.courrentTurn)
+        // console.log(this.courrentTurn)
     }
 
-    checkForReadiness(){
-        //check for game readiness
+    checkForReadiness(){ //check for game readiness
         return this.gameBoard.every(function (cell) {
             const isEmpty = cell === ''
             return isEmpty
         })
     }
 
-    checkForDraws() {
-        //check for draw, if it is full it is a draw
-        return this.gameBoard.every(function (cell) {
-            const isFull= cell !== ''
+    checkForDraws() { //check for draw, if it is full it is a draw
+        return this.gameBoard.every((cell) => {//had to use arrow functioning 
+            this.announcement = 'It\'s a draw!'
+            const isFull = cell !== ''
             return isFull
         })
     }
 
     checkForVictories(){
         //check for readiness???
-        var topLeft = this.gameBoard[0];
-        var topCenter = this.gameBoard[1];
-        var topRight = this.gameBoard[2];
-        var centerLeft = this.gameBoard[3];
-        var centerCenter= this.gameBoard[4];
-        var centerRigth = this.gameBoard[5];
-        var buttomLeft = this.gameBoard[6];
-        var buttomCenter = this.gameBoard[7];
-        var buttomRigth = this.gameBoard[8];
-        
         //check for win => if it meets one of the conditions is a win
-        if (topLeft === topCenter && topLeft === topRight){
-            console.log('the winner is =>', topLeft)
-            //invoke resetGame
+        //when a game is won playes cannot place token on the grid
+        if (this.gameBoard[0]=== this.gameBoard[1] && this.gameBoard[0] === this.gameBoard[2]){
+             //     [0, 1, 2], //checks for the top row
+            console.log('the winner is =>', this.gameBoard[0])
+            // this.resetGame();
             return;
         }
-        if (centerLeft === centerCenter && centerLeft === centerRigth){
-            console.log('the winner is => ',centerLeft)
-            //invoke resetGame
+        if (this.gameBoard[3] === this.gameBoard[4] && this.gameBoard[3] === this.gameBoard[5]){
+            //     [3, 4, 5], //checks for the middle row
+            console.log('the winner is => ',this.gameBoard[3])
+            // this.resetGame();
             return;
         }
-        if (buttomLeft === buttomCenter && buttomLeft === buttomRigth){
-            console.log('the winner is => ', buttomLeft)
-            //invoke resetGame
+        if (this.gameBoard[6] === this.gameBoard[7] && this.gameBoard[6] === this.gameBoard[8]){
+            //   [6, 7, 8], // checks for the botton row
+            console.log('the winner is => ', this.gameBoard[6])
+            // this.resetGame();
             return;
         }
-        if (centerLeft === centerCenter && centerLeft === centerRigth){
-            console.log('the winner is =>', centerLeft)
-            //invoke resetGame
+        if (this.gameBoard[0]  === this.gameBoard[3]&& this.gameBoard[0] === this.gameBoard[6]){
+            //     [0, 3, 6], // checks for the left colum
+            console.log('the winner is => ', this.gameBoard[0])
+            // this.resetGame();
             return;
         }
-        if (buttomLeft === buttomCenter && buttomLeft === buttomRight){
-            console.log('the winner is =>', buttomLeft)
-            //invoke resetGame
+        if (this.gameBoard[1] === this.gameBoard[4]&& this.gameBoard[1]=== this.gameBoard[7]){
+            //     [1, 4, 7], // checks for the middle column 
+            console.log('the winner is =>', this.gameBoard[1])
+            // this.resetGame();
             return;
         }
-        if (topLeft  === centerCenter && topleft === buttomRigth){
-            console.log('the winner is => ', centerCenter )
-            //invoke resetGame
+        if (this.gameBoard[2] === this.gameBorad[5] && this.gameBoard[2] === this.gameBoard[8]){
+            //     [2, 5, 8], // checks for the right column
+            console.log('the winner is =>', this.gameBoard[2])
+            // this.resetGame();
             return;
         }
-        if (topRight === centerCenter && topRight === buttomLeft){
-            console.log('the winner is =>', centerCenter)
-            //invoke resetGame
+        
+        if ( this.gameBoard[0] === this.gameBoard[4] && this.gameBoard[0] === this.gameBoard[8]){
+            //     [0, 4, 8], // checks 1st cross
+            console.log('the winner is =>', this.gameBoard[0])
+            // this.resetGame(); 
+            return;
+        }
+        if (this.gameBoard[2] === this.gameBoard[4] && this.gameBoard[2] === this.gameBoard[6]){
+            //     [2, 4, 6]  // checks 2nd cross
+            console.log('the winner is =>', this.gameBoard[2])
+            // this.resetGame(); 
             return;
         }
     }
+
 
     updateAnnouncement(player){//needs to make use of a paramter so it can interpolate the player
        if (checkForVictories()){
