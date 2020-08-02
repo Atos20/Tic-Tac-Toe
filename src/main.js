68feef695f917a8047dc50fe3d03dfb9d5a18a77
Orlando Murcio <ato.murcio@gmail.com>
2020-08-02 08:21:16 -0600

    Instantiate game to play on the console

--- src/main.js
+++ src/main.js
@@ -1,21 +1,7 @@
-var board = ['', '', '', '', '', '', '', '', '']
-
+// var board = ['🌚', '🌚', '🌚', '', '🌝', '', '', '🌚', '🌝'];
+// var board = ['🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚'];
+var board = ['', '', '', '', '', '', '', '', ''];
+// var board = ['🌚', '', '', '🌝', '🌚', '🌝', '', '', '🌚'];
 
 var game = new Game(board)
 console.log(game.printBoard())
-
-
-  // placeToken(player){
-    //     if(player.id === '1' && currentTurn === 'player1') {
-    //         //I think this method could be better use as a funtion that manipuates the data.
-    // }
-
-    //     //being able to provide tokens to the the board => this function willbe ablo to change the elements on the board
-    // }
-
-    //handleWin(){
-        //one of the players has succesfully met one of the winnng conditions
-        //when one of the player has met one of the winnig condition
-        // the other player cannot place a token on the grid
-        //
-    //}
