215ef8c2ccd84f3053a148e464b4b49f1f67658f
Orlando Murcio <ato.murcio@gmail.com>
2020-08-02 20:15:27 -0600

    Find winner

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 var gameBoard = document.querySelector('.game-board');
 var gameCells = document.querySelectorAll('.cell');
 var mainHeader = document.querySelector('.main-header');
+var oneWins = document.querySelector('.p1-win-count')
+var twoWins = document.querySelector('.p2-win-count')
 
 var currentGame;
 window.onload = startGame();
@@ -10,25 +12,32 @@ gameBoard.addEventListener('click', updateBoard);
 
 function startGame(){
   currentGame = new Game()
+  retrievePlayerWins()
 }
 
 function updateBoard(event){
   var cellPosition = +event.target.id
     currentGame.insertToken(cellPosition)
     gameCells[cellPosition].innerHTML = currentGame.gameBoard[cellPosition]
-    currentGame.checkWin()
+    findWinner()
     updateHeader(event)
 }
 
-function updateHeader(event){
-  var currentToken = event.target.innerHTML
+function findWinner(){
+
+  currentGame.checkWin()
+}
+
+function updateHeader(){
   var currentPlayer = currentGame.findCurrentTurn()
-  // mainHeader.innerText = `${currentGame.announcement}`
-  console.log(currentPlayer)
-  if (currentPlayer.id ===  '1' ){
-    mainHeader.innerHTML = `It's 👾's turn`
-  } else {
-    mainHeader.innerHTML = `It's 🐔's turn`
+  if (+currentPlayer.id === 1 ){
+    mainHeader.innerHTML = `It's 👾's turn`;
+  } else if (+currentPlayer.id === 2 ){
+    mainHeader.innerHTML = `It's 🐔's turn`;
   }
 }
 
+function retrievePlayerWins(){
+  oneWins.innerText = `${currentGame.player1.winCount} wins !!`
+  twoWins.innerText = `${currentGame.player2.winCount} wins !!`
+}
