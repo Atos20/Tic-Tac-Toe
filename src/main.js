345d416bcd4e97570fb0ffa923bacf977c304f28
Orlando Murcio <ato.murcio@gmail.com>
2020-08-02 18:23:06 -0600

    Update Board

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
-// var board = ['🌚', '🌚', '🌚', '', '🌝', '', '', '🌚', '🌝'];
-// var board = ['🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚', '🌝', '🌚'];
-var board = ['', '', '', '', '', '', '', '', ''];
-// var board = ['🌚', '', '', '🌝', '🌚', '🌝', '', '', '🌚'];
+//global variables
+var gameBoard = document.querySelector('.game-board');
+var gameCells = document.querySelectorAll('.cell');
+var mainHeader = document.querySelector('.main-header');
 
-var game = new Game(board)
-console.log(game.printBoard())
+var currentGame;
+window.onload = startGame();
+//event Listeners
+gameBoard.addEventListener('click', updateBoard);
+
+function startGame(){
+  currentGame = new Game()
+}
+
+function updateBoard(event){
+  var cellPosition = +event.target.id
+    currentGame.insertToken(cellPosition)
+    gameCells[cellPosition].innerHTML = currentGame.gameBoard[cellPosition]
+    currentGame.checkWin()
+    updateHeader(event)
+}
+
+function updateHeader(event){
+  var currentToken = event.target.innerHTML
+  var currentPlayer = currentGame.findCurrentTurn()
+  // mainHeader.innerText = `${currentGame.announcement}`
+  console.log(currentPlayer)
+  if (currentPlayer.id ===  '1' ){
+    mainHeader.innerHTML = `It's 👾's turn`
+  } else {
+    mainHeader.innerHTML = `It's 🐔's turn`
+  }
+}
+
